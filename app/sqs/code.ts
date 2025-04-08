8203b8169ee08034b35db05e71ae37531e3eb8e6
nicol-joseph <nicoljoseph06@icloud.com>
2025-04-08 18:12:18 +0100

    Create code.ts

---
+++ app/sqs/code.ts
@@ -0,0 +1,44 @@
+
+
+
+import * as aws from "@pulumi/aws";
+import { pdfProcessingLambda } from '../lambda';
+
+const deadLetterQueue = new aws.sqs.Queue("deadLetterQueue", {
+    name: "deadLetterQueue",
+    visibilityTimeoutSeconds: 120, 
+    messageRetentionSeconds: 604800, 
+  
+  // Remove messages in DLQ after 7 Days
+
+});
+
+const pdfProcessingQueue = new aws.sqs.Queue("pdfProcessingQueue", {
+    name: "pdfProcessingQueue",
+    
+    // Within 1 minute = Create PDF, Upload to S3, Send message, Delete message from queue
+    
+    visibilityTimeoutSeconds: 60,
+    messageRetentionSeconds: 86400, 
+  
+    // Remove messages still in the queue after 24 hours
+    
+  fifoQueue: false,
+    delaySeconds: 10, 
+  
+  // Delay message for 10 seconds to avoid possible Lambda limits.
+  
+  
+  redrivePolicy: deadLetterQueue.arn.apply((arn) => JSON.stringify({
+        deadLetterTargetArn: arn,
+        maxReceiveCount: 3,
+    }))
+});
+
+pdfProcessingQueue.onEvent("pdfProcessingQueueEvent", pdfProcessingLambda);
+
+export const Queues = {
+    deadLetterQueue,
+    pdfProcessingQueue,
+}
+;
