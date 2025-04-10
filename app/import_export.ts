7de60f9bb7a2f7f61434b036f7c011428142f2f1
nicol-joseph <nicoljoseph06@icloud.com>
2025-04-10 20:46:37 +0100

    Create import_export.ts

---
+++ app/import_export.ts
@@ -0,0 +1,7 @@
+
+import { Queues } from "./sqs";
+import { apiGateway } from "./api";
+
+export const dlqUrl = Queues.deadLetterQueue.url;
+export const pdfProcessingQueueUrl = Queues.pdfProcessingQueue.url;
+export const apiGatewayUrl = apiGateway.url;
